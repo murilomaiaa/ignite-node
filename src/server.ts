aed6d14e74b01e3bb41d5ea6ab0e608576173a0d
Murilo Maia <murilomaia.bb@gmail.com>
2021-03-26 22:13:09 -0300

    log port on terminal

--- src/server.ts
+++ src/server.ts
@@ -7,4 +7,6 @@ app.use(express.json())
 
 app.use('/categories', categoriesRoutes)
 
-app.listen(3333, () => console.log("Server is running!"))
+const port = process.env.PORT || 3333
+
+app.listen(port, () => console.log(`Server is running on port ${port}!`))
